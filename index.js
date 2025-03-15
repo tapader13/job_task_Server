542d19b4506b2420772d583dbf75ac4c90e813cd
tapader13 <minhajtapader0@gmail.com>
2025-03-14 23:45:03 -0400

    changes in url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://job-task-client-bc24f.firebaseapp.com',
     credentials: true,
   })
 );
